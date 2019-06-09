3c6495342ff9744d3c15d9e306816dafa8128638
Germantru98 <germantruhanov@gmail.com>
2019-06-09 17:42:56 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Epam_Task5
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите скобочную последовательность с клавиатуры");
+            var brackets = Console.ReadLine();
+            var checker = new BracketsChecker();
+            foreach (var ch in brackets)
+            {
+                checker.Put(ch);
+            }
+            if (checker.Balanced)
+            {
+                Console.WriteLine("Верная скобочная последовательность");
+            }
+            else
+            {
+                Console.WriteLine("Неверная скобочная последовательность");
+            }
+        }
+    }
+}
